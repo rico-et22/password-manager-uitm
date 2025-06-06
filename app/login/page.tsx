a57a8b328b9cbf09bba9e6722162e3b03dd520b8
Jakub Karaś <92886013+KarasJakub@users.noreply.github.com>
2025-06-06 21:06:06 +0200

    Shadcn config components
    * added shadcn
    
    * added other ui components

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,8 @@
-import Link from 'next/link';
-import { Form } from 'app/form';
-import { signIn } from 'app/auth';
-import { SubmitButton } from 'app/submit-button';
+import Link from "next/link"
+import { Form } from "app/form"
+import { signIn } from "app/auth"
+import { SubmitButton } from "app/submit-button"
+import { Button } from "@/components/ui/button"
 
 export default function Login() {
   return (
@@ -15,12 +16,12 @@ export default function Login() {
         </div>
         <Form
           action={async (formData: FormData) => {
-            'use server';
-            await signIn('credentials', {
-              redirectTo: '/protected',
-              email: formData.get('email') as string,
-              password: formData.get('password') as string,
-            });
+            "use server"
+            await signIn("credentials", {
+              redirectTo: "/protected",
+              email: formData.get("email") as string,
+              password: formData.get("password") as string,
+            })
           }}
         >
           <SubmitButton>Sign in</SubmitButton>
@@ -29,10 +30,10 @@ export default function Login() {
             <Link href="/register" className="font-semibold text-gray-800">
               Sign up
             </Link>
-            {' for free.'}
+            {" for free."}
           </p>
         </Form>
       </div>
     </div>
-  );
+  )
 }
