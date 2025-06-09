31e66313c49b88b15a3bbd1b474367b1e4cd0620
Kamil Pawlak <ricoet22@gmail.com>
2025-06-09 21:09:42 +0200

    add totp lib, light mode only

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="light">
       <body className={GeistSans.variable}>
         {children}
         <Toaster richColors />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,4 +7,5 @@ export default {
     hoverOnlyWhenSupported: true,
   },
   plugins: [],
+  darkMode: 'class',
 } satisfies Config;
